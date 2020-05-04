99ad2bc5b6e7252fb8ff81492cc9047f556470af
Alexandra Pryakhina <eissa.2310@gmail.com>
2020-05-04 23:00:56 +0300

    fix heroku settings

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 let _database = null;
 
-MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
+MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost/meowchat23', (err, client) => {
     if (err) {
         console.log(err);
     }else{
