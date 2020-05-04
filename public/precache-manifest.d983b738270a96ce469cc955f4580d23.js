6f0a491c4267b65f4f436f824e22ff95f1bba45d
Alexandra Pryakhina <eissa.2310@gmail.com>
2020-05-04 22:47:39 +0300

    fix heroku settings

---
+++ public/precache-manifest.d983b738270a96ce469cc955f4580d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a900bdce00e8d1cced6fe1e723882b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a2d5adfc91d01176096",
+    "url": "/static/css/main.b7583ea9.chunk.css"
+  },
+  {
+    "revision": "4a411721025356e742e3",
+    "url": "/static/js/2.a3c341f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3c341f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2d5adfc91d01176096",
+    "url": "/static/js/main.f00eadc8.chunk.js"
+  },
+  {
+    "revision": "7a0d5f8868d9050e55e9",
+    "url": "/static/js/runtime-main.e21d0e6a.js"
+  }
+]);
